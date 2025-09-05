29be6fefdae0865bd470bdcd015b2003c18512b1
Xavier Tan <164714627+XavierTann@users.noreply.github.com>
2025-09-05 14:50:46 +0800

    Added new video and itch.io link, fixed up some links

--- src/data.ts
+++ src/data.ts
@@ -15,10 +15,10 @@ export const PersonalInfo: AboutMeData = {
   description:
     "I'm super passionate about video games, both playing and making them. I love coding, and I'm always diving into new game tech and design ideas that can really level up how I build games. Right now, I am on the lookout for opportunities at game dev studios where I can grow, learn, and help create awesome experiences. ",
   image: `${process.env.PUBLIC_URL}/images/Profile.jpg`,
-  cvUri: `${process.env.PUBLIC_URL}/files/YOUR_CV.pdf`,
+  cvUri: `${process.env.PUBLIC_URL}/files/my_cv.pdf`,
   links: {
     github: "https://github.com/XavierTann",
-    itchIO: "https://NAME.itch.io",
+    itchIO: "https://xaviertann.itch.io",
     linkedIn: "https://www.linkedin.com/in/xaviertanyixuan",
   },
 };
@@ -31,7 +31,7 @@ export const games: Game[] = [
       "A 2D puzzle game where players transform into their shadow to solve intricate, light-based puzzles. Developed for a game jam with a focus on level design, shadow mechanics, and a dark fantasy aesthetic.",
     genres: ["Puzzle", "2D Platformer"],
     platforms:
-      "Trailer Design, Scene Management, Level Design, AnimatorTrees, Art Direction",
+      "Trailer Design, Level Design, Art direction and Animation, Playtesting and Iterative design, Collaboration via Git.",
     engine: GameEngine.Unity,
     links: [
       {
@@ -40,7 +40,7 @@ export const games: Game[] = [
       },
       {
         source: LinkImageSource.ItchIo,
-        url: "https://bigbull007.itch.io/light-n-seek",
+        url: "https://xaviertann.itch.io/light-and-seek",
       },
     ],
     media: [
@@ -73,12 +73,96 @@ export const games: Game[] = [
   },
 
   {
+    name: "Reboot",
+    color: "#5D728A",
+    description:
+      "A 2D side-scrolling puzzle platformer where players summon illusions by rewinding time, allowing them to interact with echoes of their past actions. Features a sleek sci-fi aesthetic and collaborative development using Git.",
+    genres: ["Puzzle Platformer", "Sci-Fi"],
+    platforms:
+      "Git Collaboration, 2D Environment and Character Art, Design Documentation, Playtesting & Iteration",
+    engine: GameEngine.Unity,
+    links: [
+      {
+        source: LinkImageSource.Github,
+        url: "https://github.com/XavierTann/reboot",
+      },
+      {
+        source: LinkImageSource.ItchIo,
+        url: "https://xaviertann.itch.io/reboot",
+      },
+    ],
+    media: [
+      {
+        source: "https://www.youtube.com/embed/s3BjaO5Ylkc",
+        type: MediaType.YouTube,
+      },
+      {
+        source: "/images/games/Reboot/Screenshot_1.png",
+        type: MediaType.Image,
+      },
+
+      {
+        source: "/images/games/Reboot/Screenshot_2.png",
+        type: MediaType.Image,
+      },
+      {
+        source: "/images/games/Reboot/Screenshot_3.png",
+        type: MediaType.Image,
+      },
+      {
+        source: "/images/games/Reboot/Screenshot_4.png",
+        type: MediaType.Image,
+      },
+      {
+        source: "/images/games/Reboot/Screenshot_5.png",
+        type: MediaType.Image,
+      },
+    ],
+  },
+
+  {
+    name: "NeoArcade VR",
+    color: "#00CED1",
+    description:
+      "A VR arcade prototype built for VirtuLabs to showcase an immersive, futuristic entertainment space in Japantown, SF. Features include dynamic cherry blossom walls, multiplayer console stations, a touchscreen photo booth, and vending machines. Developed with Unity HDRP, it explores lighting workflows, 3D rigging and animation, and AI-generated assets.",
+    genres: ["VR", "Simulation", "Futuristic"],
+    platforms:
+      "Unity Real Time and Baked Lighting, 3D Environment design, AI asset creation, 3D Animation",
+    engine: GameEngine.Unity,
+    links: [
+      {
+        source: LinkImageSource.Github,
+        url: "https://github.com/XavierTann/Futuristic-Arcade", // Replace with actual GitHub if any
+      },
+      // {
+      //   source: LinkImageSource.ItchIo,
+      //   url: "https://yourusername.itch.io/neoarcadevr", // Replace with actual Itch.io if any
+      // },
+    ],
+    media: [
+      {
+        source: "https://www.youtube.com/embed/iduUDik7mbY", // Replace with your video embed links
+        type: MediaType.YouTube,
+      },
+      {
+        source: "/images/games/Arcade_Environment/Screenshot1.png",
+        type: MediaType.Image,
+      },
+      {
+        source: "/images/games/Arcade_Environment/Screenshot2.png",
+        type: MediaType.Image,
+      },
+    ],
+  },
+
+  {
     name: "KitchenChaos",
     color: "#FFA500",
     description:
       "A cooperative cooking simulation game where players must collaborate in fast-paced, chaotic kitchens to prepare and serve meals.",
     genres: ["Simulation", "Co-op", "Casual"],
-    platforms: "UI/UX Design, Unity Prefab and ScriptableObjects System",
+    platforms:
+      "UI/UX Design, Unity Basics like the Prefab and ScriptableObjects System",
     engine: GameEngine.Unity,
     links: [
       {
@@ -134,12 +218,12 @@ export const games: Game[] = [
     links: [
       {
         source: LinkImageSource.Github,
-        url: "https://github.com/YOUR_USERNAME/TechmaniaArcade", // Replace with actual GitHub link
+        url: "https://github.com/XavierTann/my-techmania", // Replace with actual GitHub link
       },
-      {
-        source: LinkImageSource.ItchIo,
-        url: "https://yourusername.itch.io/techmania-arcade", // Replace with actual Itch.io link
-      },
+      // {
+      //   source: LinkImageSource.ItchIo,
+      //   url: "https://yourusername.itch.io/techmania-arcade", // Replace with actual Itch.io link
+      // },
     ],
     media: [
       {
@@ -166,111 +250,32 @@ export const games: Game[] = [
   },
 
   {
-    name: "Reboot",
-    color: "#5D728A",
-    description:
-      "A 2D side-scrolling puzzle platformer where players summon illusions by rewinding time, allowing them to interact with echoes of their past actions. Features a sleek sci-fi aesthetic and collaborative development using Git.",
-    genres: ["Puzzle Platformer", "Sci-Fi"],
-    platforms:
-      "Github, 2D Art, Team Collaboration using Game Design Document, Playtesting & Iteration",
-    engine: GameEngine.Unity,
-    links: [
-      {
-        source: LinkImageSource.Github,
-        url: "https://github.com/XavierTann/reboot",
-      },
-      {
-        source: LinkImageSource.ItchIo,
-        url: "https://xaviertann.itch.io/reboot",
-      },
-    ],
-    media: [
-      {
-        source: "https://www.youtube.com/embed/s3BjaO5Ylkc",
-        type: MediaType.YouTube,
-      },
-      {
-        source: "/images/games/Reboot/Screenshot_1.png",
-        type: MediaType.Image,
-      },
-
-      {
-        source: "/images/games/Reboot/Screenshot_2.png",
-        type: MediaType.Image,
-      },
-      {
-        source: "/images/games/Reboot/Screenshot_3.png",
-        type: MediaType.Image,
-      },
-      {
-        source: "/images/games/Reboot/Screenshot_4.png",
-        type: MediaType.Image,
-      },
-      {
-        source: "/images/games/Reboot/Screenshot_5.png",
-        type: MediaType.Image,
-      },
-    ],
-  },
-
-  {
-    name: "NeoArcade VR",
-    color: "#00CED1",
-    description:
-      "A VR arcade prototype built for VirtuLabs to showcase an immersive, futuristic entertainment space in Japantown, SF. Features include dynamic cherry blossom walls, multiplayer console stations, a touchscreen photo booth, and vending machines. Developed with Unity HDRP, it explores lighting workflows, 3D rigging and animation, and AI-generated assets.",
-    genres: ["VR", "Simulation", "Futuristic"],
-    platforms:
-      "3D Model Rigging and Animation, Lighting and Baking, Interactive Environment Design, AI Generation for 3D Models and Materials",
-    engine: GameEngine.Unity,
-    links: [
-      {
-        source: LinkImageSource.Github,
-        url: "https://github.com/YOUR_USERNAME/NeoArcadeVR", // Replace with actual GitHub if any
-      },
-      {
-        source: LinkImageSource.ItchIo,
-        url: "https://yourusername.itch.io/neoarcadevr", // Replace with actual Itch.io if any
-      },
-    ],
-    media: [
-      {
-        source: "https://www.youtube.com/embed/iduUDik7mbY", // Replace with your video embed links
-        type: MediaType.YouTube,
-      },
-      {
-        source: "/images/games/Arcade_Environment/Screenshot1.png",
-        type: MediaType.Image,
-      },
-      {
-        source: "/images/games/Arcade_Environment/Screenshot2.png",
-        type: MediaType.Image,
-      },
-    ],
-  },
-
-  {
     name: "Forgotten Depths",
     description:
       "A survival game set in the tunnels beneath Las Vegas, where you play as a homeless person scavenging for resources and managing health. Face nightly threats like hostile encounters, cave-ins, and harsh conditions. ",
     genres: ["First Person", "Survival Horror"],
-    platforms: "3D Level Design, Inventory System",
+    platforms:
+      "3D Environment Design, Modular Inventory System using Unity Interfaces and Inheritance",
     engine: GameEngine.Unity,
     links: [
-      { source: LinkImageSource.Github, url: "https://github.com/YOUR_REPO" },
       {
-        source: LinkImageSource.ItchIo,
-        url: "https://YOUR_ITCH.itch.io/GAME_NAME",
+        source: LinkImageSource.Github,
+        url: "https://github.com/XavierTann/ForgottenDepths",
       },
+      // {
+      //   source: LinkImageSource.ItchIo,
+      //   url: "https://YOUR_ITCH.itch.io/GAME_NAME",
+      // },
     ],
     media: [
       {
         source: "/images/games/Unforgotten_Depths/Screenshot_1.png",
         type: MediaType.Image,
       },
-      // {
-      //   source: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-      //   type: MediaType.YouTube,
-      // },
+      {
+        source: "https://www.youtube.com/embed/4-A-AjeJbXY",
+        type: MediaType.YouTube,
+      },
       {
         source: "/images/games/Unforgotten_Depths/Screenshot_2.png",
         type: MediaType.Image,
@@ -291,7 +296,7 @@ export const games: Game[] = [
     description:
       "My first game! A top down RPG where players have to navigate and fight through hordes of enemies while levelling up and gaining new skills. Unfortunately I did not know how to use source control, and lost all the code. The only picture I have of the game  is this.",
     genres: ["Top Down RPG"],
-    platforms: "UI/UX Design,2D Art",
+    platforms: "UI/UX Design, 2D Art, Unity Basics",
     engine: GameEngine.Unity,
     links: [
       { source: LinkImageSource.Github, url: "https://github.com/YOUR_REPO" },
